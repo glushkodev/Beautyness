5f2687db7f128a5cf21f1503011ecac5b6ced070
Ekaterina <glushkoekaterina.vi@gmail.com>
2025-06-17 18:57:46 +0300

    added section Benefits

---
+++ src/components/pages/home-page/ui/components/Benefits/Benefits.jsx
@@ -0,0 +1,50 @@
+import styles from './Benefits.module.css';
+import Arrow from '../../../../../../assets/benefits_arrow.svg?react';
+import Border from '../../../../../../assets/benefits_border.svg?react';
+
+const Benefits = () => {
+  return (
+    <section className={styles.benefits}>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <h3 className={styles.subhead}>Наши потрясающие преимущества</h3>
+          <h2 className={styles.title}>На самом деле, что вы получите</h2>
+          <div className={styles.items}>
+            <div className={styles.item}>
+              <div className={styles.arrow}><Arrow /><Border className={styles.border} /></div>
+              <div className={styles.subtitle}>Оптимизированная энергоэффективность</div>
+              <div className={styles.subtext}>Смысл использования Lorem Ipsum в том, чтобы сделать его читабельным.</div>
+            </div>
+            <div className={styles.item}>
+              <div className={styles.arrow}><Arrow /><Border className={styles.border} /></div>
+              <div className={styles.subtitle}>Многогранная фильтрация воды</div>
+              <div className={styles.subtext}>Смысл использования Lorem Ipsum в том, чтобы сделать его читабельным.</div>
+            </div>
+            <div className={styles.item}>
+              <div className={styles.arrow}><Arrow /><Border className={styles.border} /></div>
+              <div className={styles.subtitle}>Бескомпромиссное качество</div>
+              <div className={styles.subtext}>Смысл использования Lorem Ipsum в том, чтобы сделать его читабельным.</div>
+            </div>
+            <div className={styles.item}>
+              <div className={styles.arrow}><Arrow /><Border className={styles.border} /></div>
+              <div className={styles.subtitle}>Расслабляющие спа-процедуры</div>
+              <div className={styles.subtext}>Смысл использования Lorem Ipsum в том, чтобы сделать его читабельным.</div>
+            </div>
+            <div className={styles.item}>
+              <div className={styles.arrow}><Arrow /><Border className={styles.border} /></div>
+              <div className={styles.subtitle}>Легкий уход для лучшего удовольствия</div>
+              <div className={styles.subtext}>Смысл использования Lorem Ipsum в том, чтобы сделать его читабельным.</div>
+            </div>
+            <div className={styles.item}>
+              <div className={styles.arrow}><Arrow /><Border className={styles.border} /></div>
+              <div className={styles.subtitle}>Оптимизированная энергоэффективность</div>
+              <div className={styles.subtext}>Смысл использования Lorem Ipsum в том, чтобы сделать его читабельным.</div>
+            </div>
+          </div>        
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Benefits
--- src/components/pages/home-page/ui/index.jsx
+++ src/components/pages/home-page/ui/index.jsx
@@ -1,12 +1,14 @@
 import styles from './index.module.css'
 import Welcome from "./components/welcome/Welcome"
 import About from './components/About/About'
+import Benefits from './components/Benefits/Benefits'
 
 const HomePage = () => {
 	return (
 		<div className={styles.welcome}>
 			<Welcome />
 			<About />
+			<Benefits />
 		</div>
 	)
 }
