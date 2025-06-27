3e74ac4b5a1e8964bae81d55702c1ddd3a78984c
Ekaterina <glushkoekaterina.vi@gmail.com>
2025-06-27 22:33:16 +0300

    services page is done

--- src/components/pages/services-page/ui/components/Service/Service.jsx
+++ src/components/pages/services-page/ui/components/Service/Service.jsx
@@ -1,11 +1,215 @@
 import styles from './Service.module.css'
+import Img from '../../../../../../assets/prices_img.png'
+import Img2 from '../../../../../../assets/prices_img2.png'
+import Img3 from '../../../../../../assets/prices_img3.png'
+import Img4 from '../../../../../../assets/prices_img4.png'
+import Img5 from '../../../../../../assets/prices_img5.png'
+import Bg from '../../../../../../assets/prices_bg.png'
 
 const Service = () => {
   return (
     <section className={styles.service}>
       <div className={styles.container}>
         <div className={styles.content}>
-          
+          <h3 className={styles.subhead}>Наши слуги</h3>
+          <h2 className={styles.head}>У нас есть опыт сделать вас красивыми</h2>
+          <div className={styles.elems}>
+            <img className={styles.bg} src={Bg} alt="Bg Image" />
+            <img className={styles.img} src={Img} alt="Services Image" />
+            <div className={styles.listRight}>
+              <div className={styles.list}>
+                <div className={styles.item}>
+                  <div className={styles.header}>
+                    <h4 className={styles.title}>Маски для лица</h4>
+                    <span className={styles.dots}></span>
+                    <span className={styles.price}>от 48 руб</span>
+                  </div>
+                  <div className={styles.descr}>Версии развивались с течением времени, иногда случайно.</div>
+                </div>
+                <div className={styles.item}>
+                  <div className={styles.header}>
+                    <h4 className={styles.title}>Массаж всего тела</h4>
+                    <span className={styles.dots}></span>
+                    <span className={styles.price}>от 67 руб</span>
+                  </div>
+                  <div className={styles.descr}>Версии развивались с течением времени, иногда случайно.</div>
+                </div>
+                <div className={styles.item}>
+                  <div className={styles.header}>
+                    <h4 className={styles.title}>Геотермальный спа</h4>
+                    <span className={styles.dots}></span>
+                    <span className={styles.price}>от 54 руб</span>
+                  </div>
+                  <div className={styles.descr}>Версии развивались с течением времени, иногда случайно.</div>
+                </div>
+                <div className={styles.item}>
+                  <div className={styles.header}>
+                    <h4 className={styles.title}>Расслабляющая сауна</h4>
+                    <span className={styles.dots}></span>
+                    <span className={styles.price}>от 73 руб</span>
+                  </div>
+                  <div className={styles.descr}>Версии развивались с течением времени, иногда случайно.</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.elemsRight}>
+            <div className={styles.listLeft}>
+              <div className={styles.list}>
+                <div className={styles.item}>
+                  <div className={styles.header}>
+                    <h4 className={styles.title}>Маски для лица</h4>
+                    <span className={styles.dots}></span>
+                    <span className={styles.price}>от 48 руб</span>
+                  </div>
+                  <div className={styles.descr}>Версии развивались с течением времени, иногда случайно.</div>
+                </div>
+                <div className={styles.item}>
+                  <div className={styles.header}>
+                    <h4 className={styles.title}>Массаж всего тела</h4>
+                    <span className={styles.dots}></span>
+                    <span className={styles.price}>от 67 руб</span>
+                  </div>
+                  <div className={styles.descr}>Версии развивались с течением времени, иногда случайно.</div>
+                </div>
+                <div className={styles.item}>
+                  <div className={styles.header}>
+                    <h4 className={styles.title}>Геотермальный спа</h4>
+                    <span className={styles.dots}></span>
+                    <span className={styles.price}>от 54 руб</span>
+                  </div>
+                  <div className={styles.descr}>Версии развивались с течением времени, иногда случайно.</div>
+                </div>
+                <div className={styles.item}>
+                  <div className={styles.header}>
+                    <h4 className={styles.title}>Расслабляющая сауна</h4>
+                    <span className={styles.dots}></span>
+                    <span className={styles.price}>от 73 руб</span>
+                  </div>
+                  <div className={styles.descr}>Версии развивались с течением времени, иногда случайно.</div>
+                </div>
+              </div>
+            </div>
+            <img className={styles.img} src={Img2} alt="Services Image" />
+          </div>
+          <div className={styles.elems}>
+            <img className={styles.bg} src={Bg} alt="Bg Image" />
+            <img className={styles.img} src={Img3} alt="Services Image" />
+            <div className={styles.listRight}>
+              <div className={styles.list}>
+                <div className={styles.item}>
+                  <div className={styles.header}>
+                    <h4 className={styles.title}>Маски для лица</h4>
+                    <span className={styles.dots}></span>
+                    <span className={styles.price}>от 48 руб</span>
+                  </div>
+                  <div className={styles.descr}>Версии развивались с течением времени, иногда случайно.</div>
+                </div>
+                <div className={styles.item}>
+                  <div className={styles.header}>
+                    <h4 className={styles.title}>Массаж всего тела</h4>
+                    <span className={styles.dots}></span>
+                    <span className={styles.price}>от 67 руб</span>
+                  </div>
+                  <div className={styles.descr}>Версии развивались с течением времени, иногда случайно.</div>
+                </div>
+                <div className={styles.item}>
+                  <div className={styles.header}>
+                    <h4 className={styles.title}>Геотермальный спа</h4>
+                    <span className={styles.dots}></span>
+                    <span className={styles.price}>от 54 руб</span>
+                  </div>
+                  <div className={styles.descr}>Версии развивались с течением времени, иногда случайно.</div>
+                </div>
+                <div className={styles.item}>
+                  <div className={styles.header}>
+                    <h4 className={styles.title}>Расслабляющая сауна</h4>
+                    <span className={styles.dots}></span>
+                    <span className={styles.price}>от 73 руб</span>
+                  </div>
+                  <div className={styles.descr}>Версии развивались с течением времени, иногда случайно.</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.elemsRight}>
+            <div className={styles.listLeft}>
+              <div className={styles.list}>
+                <div className={styles.item}>
+                  <div className={styles.header}>
+                    <h4 className={styles.title}>Маски для лица</h4>
+                    <span className={styles.dots}></span>
+                    <span className={styles.price}>от 48 руб</span>
+                  </div>
+                  <div className={styles.descr}>Версии развивались с течением времени, иногда случайно.</div>
+                </div>
+                <div className={styles.item}>
+                  <div className={styles.header}>
+                    <h4 className={styles.title}>Массаж всего тела</h4>
+                    <span className={styles.dots}></span>
+                    <span className={styles.price}>от 67 руб</span>
+                  </div>
+                  <div className={styles.descr}>Версии развивались с течением времени, иногда случайно.</div>
+                </div>
+                <div className={styles.item}>
+                  <div className={styles.header}>
+                    <h4 className={styles.title}>Геотермальный спа</h4>
+                    <span className={styles.dots}></span>
+                    <span className={styles.price}>от 54 руб</span>
+                  </div>
+                  <div className={styles.descr}>Версии развивались с течением времени, иногда случайно.</div>
+                </div>
+                <div className={styles.item}>
+                  <div className={styles.header}>
+                    <h4 className={styles.title}>Расслабляющая сауна</h4>
+                    <span className={styles.dots}></span>
+                    <span className={styles.price}>от 73 руб</span>
+                  </div>
+                  <div className={styles.descr}>Версии развивались с течением времени, иногда случайно.</div>
+                </div>
+              </div>
+            </div>
+            <img className={styles.img} src={Img4} alt="Services Image" />
+          </div>
+          <div className={styles.elems}>
+            <img className={styles.img} src={Img5} alt="Services Image" />
+            <div className={styles.listRight}>
+              <div className={styles.list}>
+                <div className={styles.item}>
+                  <div className={styles.header}>
+                    <h4 className={styles.title}>Маски для лица</h4>
+                    <span className={styles.dots}></span>
+                    <span className={styles.price}>от 48 руб</span>
+                  </div>
+                  <div className={styles.descr}>Версии развивались с течением времени, иногда случайно.</div>
+                </div>
+                <div className={styles.item}>
+                  <div className={styles.header}>
+                    <h4 className={styles.title}>Массаж всего тела</h4>
+                    <span className={styles.dots}></span>
+                    <span className={styles.price}>от 67 руб</span>
+                  </div>
+                  <div className={styles.descr}>Версии развивались с течением времени, иногда случайно.</div>
+                </div>
+                <div className={styles.item}>
+                  <div className={styles.header}>
+                    <h4 className={styles.title}>Геотермальный спа</h4>
+                    <span className={styles.dots}></span>
+                    <span className={styles.price}>от 54 руб</span>
+                  </div>
+                  <div className={styles.descr}>Версии развивались с течением времени, иногда случайно.</div>
+                </div>
+                <div className={styles.item}>
+                  <div className={styles.header}>
+                    <h4 className={styles.title}>Расслабляющая сауна</h4>
+                    <span className={styles.dots}></span>
+                    <span className={styles.price}>от 73 руб</span>
+                  </div>
+                  <div className={styles.descr}>Версии развивались с течением времени, иногда случайно.</div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
